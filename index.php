fc00f46ecaf3ae3ea6dac757858da07f91ce6257
Célien <71440955+CELAULT@users.noreply.github.com>
2021-09-28 10:33:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+        <title>PPL</title>
+    </head>
+
+    <body>
+        <center>
+            <h1>PHP POO LOGIN</h1>
+
+            <?php
+
+            ?>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+        <center>
+    </body>
+</html>
