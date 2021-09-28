dc829b87ea47596ecaa7aaafd39351832753bc17
Célien <71440955+CELAULT@users.noreply.github.com>
2021-09-28 16:06:41 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,23 +1,47 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/signin.css">
+    <title>LOGIN</title>
+  </head>
 
-        <title>PPL</title>
-    </head>
+  <body>
+      <main class="form-signin">
+        <form method="POST">
+          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-    <body>
-        <center>
-            <h1>PHP POO LOGIN</h1>
+          <div class="form-floating">
+            <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
 
-            <?php
+            <label for="floatingInput">Email address</label>
+          </div>
 
-            ?>
+          <div class="form-floating">
+            <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
 
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-        <center>
-    </body>
+            <label for="floatingPassword">Password</label>
+          </div>
+
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"> Remember me
+            </label>
+          </div>
+
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+          <p class="mt-5 mb-3 text-muted"></p>
+        </form>
+      </main>
+  </body>
 </html>
+
+<?php
+include 'header.php';
+
+print("<br/>email = '" . $_POST['email'] . "'");
+?>
